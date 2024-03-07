d5dc2cc2f990e8d67c9f7f9a291279c5ec431612
Yuliia <the.smile.too@gmail.com>
2024-03-07 11:48:40 +0200

    1. displaying the task on the page 2. function of clearing the input field

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,14 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+};
+
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
 );
@@ -32,13 +40,28 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+
+  const taskObj = {
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value,
+  };
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
+  }
+  taskData.forEach(({ id, title, date, description }) => {
+    tasksContainer.innerHTML += `
+    <div class='task' id ='${id}'>
+        <p><strong>Title:</strong> ${title}</p>
+        <p><strong>Date:</strong>${date}</p>
+        <p><strong>Description:</strong>${description}</p>
+        <button type="button" class="btn">Edit</button>
+        <button type="button" class="btn">Delete</button> 
+    </div> 
+    `;
+  });
+  taskForm.classList.toggle("hidden");
 });
-
-const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-
-const taskObj = {
-  id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-  title: titleInput.value,
-  date: dateInput.value,
-  description: descriptionInput.value,
-};
