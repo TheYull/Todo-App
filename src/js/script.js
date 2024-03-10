9d75988198b86043b7ff395bef714be80b2d9f11
Yuliia <the.smile.too@gmail.com>
2024-03-10 19:52:25 +0200

    update the delete function

--- src/js/script.js
+++ src/js/script.js
@@ -48,6 +48,7 @@ const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
   );
+  buttonEl.parentElement.remove();
 };
 
 const reset = () => {
