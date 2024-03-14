805356548ef7002ede43b5bfa7ba4850d9ce81e2
Yuliia <the.smile.too@gmail.com>
2024-03-14 12:00:31 +0200

    data storage function, finalization of other functions

--- src/js/script.js
+++ src/js/script.js
@@ -10,7 +10,7 @@ const titleInput = document.getElementById("title-input");
 const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
-const taskData = [];
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
 let currentTask = {};
 
 const addOrUpdateTask = () => {
@@ -54,6 +54,7 @@ const deleteTask = (buttonEl) => {
   );
   buttonEl.parentElement.remove();
   taskData.splice(dataArrIndex, 1);
+  localStorage.setItem("data", JSON.stringify(taskData));
 };
 
 const editTask = (buttonEl) => {
@@ -77,6 +78,10 @@ const reset = () => {
   taskForm.classList.toggle("hidden");
   currentTask = {};
 };
+
+if (taskData.length) {
+  updateTaskContainer();
+}
 
 openTaskFormBtn.addEventListener("click", () =>
   taskForm.classList.toggle("hidden")
